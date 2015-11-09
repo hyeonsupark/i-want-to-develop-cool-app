d56df531de6b4d815dec418eb12732c4ad5b1f51
Hyeonsu Park <bingraa@gmail.com>
2015-11-09 12:05:09 +0900

    Test Retrofit and Lambda

--- app/src/main/java/kr/applepi/coolapp/activities/MainActivity.java
+++ app/src/main/java/kr/applepi/coolapp/activities/MainActivity.java
@@ -1,18 +1,49 @@
 package kr.applepi.coolapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import kr.applepi.coolapp.R;
+import kr.applepi.coolapp.controllers.Github;
+import kr.applepi.coolapp.models.GithubUser;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnHi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHi = (Button) findViewById(R.id.btn_hi);
+
+        btnHi.setOnClickListener((view) -> {
+            Retrofit githubRetro = new Retrofit.Builder()
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://api.github.com/")
+                    .build();
+
+            Github github = githubRetro.create(Github.class);
+            Observable<GithubUser> githubUser = github.getGithubUser("hyeonsupark");
+            githubUser.subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .map(user -> "Name : " + user.name)
+                    .subscribe(userInfo -> Toast.makeText(getApplicationContext(), userInfo, Toast.LENGTH_SHORT).show());
+        });
+
+
     }
 
     @Override
